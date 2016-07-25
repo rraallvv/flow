ce1512658216c49eefc636a33c71e452fcfdfdee
Developer <>
2016-07-25 02:04:18 -0400

    don't update the preview if not ready

--- panel/flow.js
+++ panel/flow.js
@@ -2,6 +2,75 @@
 (function() {
 	var e = require("electron"),
 		n = require("fire-url");
+
+	//*
+	var demos = [
+		{ name: "Color",
+			nodes: [
+				{type:"fragColor", pos:[650, 130]},
+				{type:"vec4", pos:[100, 100], value:[1,0,0,1]}
+			],
+			links: [
+				[1, 0]
+			]
+		},
+		{ name: "Pattern",
+			nodes: [
+				{type:"fragColor", pos:[650, 130]},
+				{type:"value", pos:[0, 0], value:70},
+				{type:"uv", pos:[0, 100]},
+				{type:"value", pos:[0, 220], value:35},
+				{type:"value", pos:[0, 320], value:0.5},
+				{type:"multiply", pos:[200, 50]},
+				{type:"multiply", pos:[200, 150]},
+				{type:"cos", pos:[350, 50]},
+				{type:"sin", pos:[350, 150]},
+				{type:"join", pos:[500, 100]},
+				{type:"value", pos:[320, 250], value: 1}
+			],
+			links: [
+				[1, 5],
+				[2.1, 5.1],
+				[2.2, 6],
+				[3, 6.1],
+				[5, 7],
+				[6, 8],
+				[7, 9],
+				[8, 9.1],
+				[4, 9.2],
+				[10, 9.3],
+				[9, 0]
+			]
+		},
+		{ name: "Black and White",
+			nodes: [
+				{type:"fragColor", pos:[660, 200]},
+				{type:"texture", pos:[0, 0]},
+				{type:"split", pos:[0, 170]},
+				{type:"add", pos:[135, 90]},
+				{type:"add", pos:[267, 90]},
+				{type:"divide", pos:[400, 90]},
+				{type:"value", pos:[250, 175], value:3},
+				{type:"join", pos:[530, 170]}
+			],
+			links: [
+				[1, 2],
+				[2, 3],
+				[2.1, 3.1],
+				[3, 4],
+				[2.2, 4.1],
+				[4, 5],
+				[6, 5.1],
+				[5, 7],
+				[5, 7.1],
+				[5, 7.2],
+				[2.3, 7.3],
+				[7, 0]
+			]
+		}
+	];
+	//*/
+
 	Editor.polymerPanel("flow", {
 		behaviors: [ Editor.UI.Droppable ],
 		hostAttributes: {
@@ -97,7 +166,7 @@
 				prettyPrint();
 				*/
 
-				if (preview) {
+				if (preview && preview.updateShader) {
 					preview.updateShader(shaderDef);
 				}
 			};
@@ -111,74 +180,6 @@
 			shaderGraph.onConnectionStarted = function(e) {
 				menu.toggleMenuOff();
 			};
-			
-			//*
-			var demos = [
-				{ name: "Color",
-					nodes: [
-						{type:"fragColor", pos:[650, 130]},
-						{type:"vec4", pos:[100, 100], value:[1,0,0,1]}
-					],
-					links: [
-						[1, 0]
-					]
-				},
-				{ name: "Pattern",
-					nodes: [
-						{type:"fragColor", pos:[650, 130]},
-						{type:"value", pos:[0, 0], value:70},
-						{type:"uv", pos:[0, 100]},
-						{type:"value", pos:[0, 220], value:35},
-						{type:"value", pos:[0, 320], value:0.5},
-						{type:"multiply", pos:[200, 50]},
-						{type:"multiply", pos:[200, 150]},
-						{type:"cos", pos:[350, 50]},
-						{type:"sin", pos:[350, 150]},
-						{type:"join", pos:[500, 100]},
-						{type:"value", pos:[320, 250], value: 1}
-					],
-					links: [
-						[1, 5],
-						[2.1, 5.1],
-						[2.2, 6],
-						[3, 6.1],
-						[5, 7],
-						[6, 8],
-						[7, 9],
-						[8, 9.1],
-						[4, 9.2],
-						[10, 9.3],
-						[9, 0]
-					]
-				},
-				{ name: "Black and White",
-					nodes: [
-						{type:"fragColor", pos:[660, 200]},
-						{type:"texture", pos:[0, 0]},
-						{type:"split", pos:[0, 170]},
-						{type:"add", pos:[135, 90]},
-						{type:"add", pos:[267, 90]},
-						{type:"divide", pos:[400, 90]},
-						{type:"value", pos:[250, 175], value:3},
-						{type:"join", pos:[530, 170]}
-					],
-					links: [
-						[1, 2],
-						[2, 3],
-						[2.1, 3.1],
-						[3, 4],
-						[2.2, 4.1],
-						[4, 5],
-						[6, 5.1],
-						[5, 7],
-						[5, 7.1],
-						[5, 7.2],
-						[2.3, 7.3],
-						[7, 0]
-					]
-				}
-			];
-			//*/
 
 			/*
 			var placeholder = document.getElementById("demos");
